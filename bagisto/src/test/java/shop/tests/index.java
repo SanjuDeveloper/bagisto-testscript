99dcbefb939077e0d30227a67d795949b87d311c
anonymousauthentication <manoharkant.joshi@gmail.com>
2022-11-24 01:09:17 +0530

    code refector

--- bagisto/src/test/java/shop/tests/index.java
+++ bagisto/src/test/java/shop/tests/index.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import org.testng.annotations.Test;
 
 import shop.TestComponents.*;
-import shop.pageobjects.ProductListing;
+
 
 
 public class index extends baseTest {
