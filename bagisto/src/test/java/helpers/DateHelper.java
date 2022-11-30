dd07f4df9da3cce8be995b0bef01296a90032d59
SanjuDeveloper <bhattsanju41@gmail.com>
2022-11-30 09:12:47 -0800

    Date Helper Function Created

---
+++ bagisto/src/test/java/helpers/DateHelper.java
@@ -0,0 +1,25 @@
+package helpers;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class DateHelper {
+
+	public static void main(String[] args) {
+		System.out.println(getDate("TODAY"));
+
+	}
+	
+	public static  String getDate(String rquestType) {		
+		Calendar calendar = Calendar.getInstance();  // get a calendar instance, which defaults to "now"	   	
+		 Date gettype = calendar.getTime();
+		 
+		if(rquestType=="AFTER_DATE") {
+			calendar.add(Calendar.DAY_OF_YEAR, 5);
+			 gettype = calendar.getTime();
+		}
+		
+		return  new SimpleDateFormat("d").format(gettype);	   
+	}
+
+}
