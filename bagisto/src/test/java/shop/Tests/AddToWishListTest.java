6cc08652f08295f5450b27e3cfd8c14f3c642816
anonymousauthentication <manoharkantjohi@gmail.com>
2023-01-03 22:14:20 +0530

    CodeRefector

--- bagisto/src/test/java/shop/Tests/AddToWishListTest.java
+++ bagisto/src/test/java/shop/Tests/AddToWishListTest.java
@@ -9,10 +9,11 @@ import common.BaseTest;
 public class AddToWishListTest {
 	@Test(dependsOnGroups = {"LaunchShopTest.openBrowser","CustomerLoginTest.customerLogin"})
 	public void addToWishlist() throws InterruptedException, IOException {
-		ProductActionHelper helper = new ProductActionHelper(driver);
+		ProductActionHelper helper = new ProductActionHelper(BaseTest.driver);
 		helper.homeLogo.click();
 		String success_message = helper.addProductTo("Wishlist",1);	
 		System.out.println(success_message);
 		Assert.assertEquals(success_message, "Success! Item Successfully Added To Wishlist.");
+
 	}
 }
