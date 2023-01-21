3e1bbe3b1c979ca9b859916ea0a8df8c729be4fd
anonymousauthentication <manoharkantjohi@gmail.com>
2023-01-21 17:44:13 +0530

    Flipkart code optimize;

--- bagisto/src/test/java/shop/Tests/FlipkartProduct.java
+++ bagisto/src/test/java/shop/Tests/FlipkartProduct.java
@@ -1,8 +1,8 @@
 package shop.Tests;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -10,9 +10,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import common.BaseTest;
 
-public class FlipkartProduct extends BaseTest {
+public class FlipkartProduct  {
 	@Test
 	public void flipKart() throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
@@ -24,35 +23,16 @@ public class FlipkartProduct extends BaseTest {
 		List<WebElement> allProducts = driver.findElements(By.cssSelector("div[class=\"_13oc-S\"]"));
 		List<WebElement> productName = driver.findElements(By.cssSelector("div[class=\"_4rR01T\"]"));
 		List<WebElement> productPrice = driver.findElements(By.cssSelector("div[class=\"_30jeq3 _1_WHN1\"]"));
-	
-		System.out.println(allProducts.size());
 		for (int i = 0; i < allProducts.size(); i++) {
-			int price = remove$(productPrice.get(i).getText());
+			int price = Integer.parseInt(productPrice.get(i).getText().replaceAll("[^a-zA-Z0-9.]", ""));
 			ss.add(price);
 		}
-		int smallestPrice = getSmallest(ss);
-		System.out.println(smallestPrice);
+		int smallestPrice = Collections.min(ss);
 		for (int i = 0; i < allProducts.size(); i++) {
-			if(smallestPrice == remove$(productPrice.get(i).getText())){
+			if(smallestPrice == Integer.parseInt(productPrice.get(i).getText().replaceAll("[^a-zA-Z0-9.]", ""))){
 				System.out.println(productName.get(i).getText());
-				System.out.println(remove$(productPrice.get(i).getText()));
+				System.out.println(Integer.parseInt(productPrice.get(i).getText().replaceAll("[^a-zA-Z0-9.]", "")));
 			}
 		}
-	}
-
-	public int remove$(String price) {
-		int actualPrice = (int) Float.parseFloat(price.replaceAll("[^a-zA-Z0-9.]", ""));
-		return actualPrice;
-	}
-
-	public static int getSmallest(ArrayList ss) {
-		int min = (int) ss.get(0);
-		System.out.println("size"+ss.size());
-		for (int i = 0; i < ss.size(); i++) {
-			if ((int) ss.get(i) < min) {
-				min = (int) ss.get(i);
-			}
-		}
-		return min;
 	}
 }
